55067a809b5b4da80a59f3de1bb9a6948ff4940a
Luca Barco <luca.barco@linksfoundation.com>
2025-10-16 18:46:57 +0200

    (#18) add EUMETSAT Authenticator

--- src/satctl/auth/__init__.py
+++ src/satctl/auth/__init__.py
@@ -1,10 +1,12 @@
 from satctl.auth.base import Authenticator
 from satctl.auth.earthdata import EarthDataAuthenticator
+from satctl.auth.eumetsat import EUMETSATAuthenticator
 from satctl.auth.odata import ODataAuthenticator
 from satctl.registry import Registry
 
 registry = Registry[Authenticator](name="authenticator")
 registry.register("odata", ODataAuthenticator)
 registry.register("earthaccess", EarthDataAuthenticator)
+registry.register("eumetsat", EUMETSATAuthenticator)
 
-__all__ = ["ODataAuthenticator", "EarthDataAuthenticator"]
+__all__ = ["ODataAuthenticator", "EarthDataAuthenticator", "EUMETSATAuthenticator"]
