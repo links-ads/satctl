e1fe29b0f46d7d798bfedfd2597994353778ce02
gaetanochiriaco <gaetano.chiriaco@linksfoundation.com>
2025-10-17 15:11:16 +0200

    (#17)  added first draft of sentinel2 l1c and l2a sources with composites and tests

--- src/satctl/sources/__init__.py
+++ src/satctl/sources/__init__.py
@@ -7,6 +7,7 @@ from satctl.registry import Registry
 from satctl.sources.base import DataSource
 from satctl.sources.modis import MODISL1BSource
 from satctl.sources.mtg import MTGSource
+from satctl.sources.sentinel2 import Sentinel2L1CSource, Sentinel2L2ASource
 from satctl.sources.sentinel3 import OLCISource, SLSTRSource
 from satctl.sources.viirs import VIIRSL1BSource
 
@@ -16,6 +17,8 @@ registry.register("olci", OLCISource)
 registry.register("viirs-l1b", VIIRSL1BSource)
 registry.register("mtg", MTGSource)
 registry.register("modis-l1b", MODISL1BSource)
+registry.register("s2l2a", Sentinel2L2ASource)
+registry.register("s2l1c", Sentinel2L1CSource)
 
 
 def create_source(
@@ -57,11 +60,4 @@ def create_source(
     )
 
 
-__all__ = [
-    "OLCISource",
-    "SLSTRSource",
-    "VIIRSL1BSource",
-    "MTGSource"
-    "MODISL1BSource",
-    "create_source",
-]
+__all__ = ["OLCISource", "SLSTRSource", "Sentinel2L2ASource", "Sentinel2L1CSource", "MTGSource" "create_source"]
