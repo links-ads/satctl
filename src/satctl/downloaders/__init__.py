a95eca2d77491f8e11400c71ba9307d65e6649f8
gaetanochiriaco <gaetano.chiriaco@linksfoundation.com>
2025-10-20 12:07:48 +0200

    (#17 #19) added S3 auth and downloader class

--- src/satctl/downloaders/__init__.py
+++ src/satctl/downloaders/__init__.py
@@ -1,11 +1,14 @@
 from satctl.downloaders.base import Downloader
 from satctl.downloaders.http import HTTPDownloader
+from satctl.downloaders.s3 import S3Downloader
 from satctl.registry import Registry
 
 registry = Registry[Downloader](name="downloader")
 registry.register("http", HTTPDownloader)
+registry.register("s3", S3Downloader)
 
 __all__ = [
     "Downloader",
     "HTTPDownloader",
+    "S3Downloader",
 ]
